f96a320110bd2ce1b51a110544f4504f7ae6d1bd
Stoyak <97674215+alexanderStoyak@users.noreply.github.com>
2023-09-23 23:14:01 +0300

    Create userScript.js

---
+++ userScript.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Search Chat
+// @namespace    http://tampermonkey.net/
+// @description  Поисковик бесед ВКонтакте в Вашем браузере
+// @author       Alexander Stoyak
+// @match        https://vk.com/*
+// @connect      vk.com
+// @connect      vk-cdn.com
+// @connect      vk-cdn.net
+// @connect      userapi.com
+// @connect      cdnjs.cloudflare.com
+// @connect      login.vk.com
+// @connect      oauth.vk.com
+// @connect      api.vk.com
+// @connect      localhost
+// @connect      api.search-for-chats-of-vk.ru
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/main.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/handlers/buttonInProfiles.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/API.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/blanksHTML.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/request.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/services.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/styles.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/utils/VKFunctions.js
+
+// @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @version      0.0.1
+// ==/UserScript==
