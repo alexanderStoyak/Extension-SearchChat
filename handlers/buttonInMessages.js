90b9b234e6b09647826f03cda4ce1c8364383321
Stoyak <97674215+alexanderStoyak@users.noreply.github.com>
2023-10-03 17:55:54 +0300

    fix bug
    fixed a bug where there was no chats button in messages when excluding or exiting a chat

--- handlers/buttonInMessages.js
+++ handlers/buttonInMessages.js
@@ -11,13 +11,13 @@ async function buttonInMessages(peerHistory) {
                 const newAction = document.createElement('span');
 
                 newAction.id = 'buttonInMessages';
-                newAction.className = 'im-mess--reaction'
-                newAction.onclick = () => userOrGropChats(href, 0);
+                newAction.className = 'im-mess--fav'
                 newAction.role = 'link';
                 newAction.title = 'Просмотр чатов';
                 newAction.innerHTML = icons({name: 'chats', fill: 'secondary', size: 16});
 
                 const [{href}] = message.getElementsByClassName('im-mess-stack--lnk');
+                newAction.onclick = () => userOrGropChats(href, 0);
 
                 action.prepend(newAction);
             }
