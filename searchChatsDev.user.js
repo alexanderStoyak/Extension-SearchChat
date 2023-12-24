581279db77a7cad51d43eb90fc04589178fdb800
Stoyak <97674215+alexanderStoyak@users.noreply.github.com>
2023-12-24 18:29:02 +0300

    Create searchChatsDev.user.js

---
+++ searchChatsDev.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         ПоискЧата
+// @namespace    http://tampermonkey.net/
+// @description  Поисковик бесед ВКонтакте в Вашем браузере
+// @author       Alexander Stoyak
+// @license      MIT
+// @icon         https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/icons/logo.png
+// @match        https://vk.com/*
+
+// @connect      vk.com
+// @connect      vk-cdn.com
+// @connect      vk-cdn.net
+// @connect      userapi.com
+// @connect      cdnjs.cloudflare.com
+// @connect      login.vk.com
+// @connect      oauth.vk.com
+// @connect      api.vk.com
+// @connect      api.search-for-chats-of-vk.ru
+// @connect      momentjs.com
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/classes.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/main.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/icons.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/handlers/buttonInProfiles.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/handlers/buttonInMessages.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/handlers/buttonInProfilesForGroups.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/handlers/buttonInDialogsSearch.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/handlers/buttonsInTopProfileMenu.js
+
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/blanksHTML.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/request.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/services.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/styles.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/functions.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/API.js
+// @require      https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/utils/onClicks.js
+
+// @require      https://momentjs.com/downloads/moment-with-locales.js
+
+// @run-at       document-start
+
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+
+// @updateURL    https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/main/searchChats.user.js
+// @downloadURL  https://raw.githubusercontent.com/alexanderStoyak/Extension-SearchChat/dev/searchChats.user.js
+
+// @version      5.5.10
+
+// ==/UserScript==
